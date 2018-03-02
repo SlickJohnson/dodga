8203dad897cb2478b2a43e1215316edcd27c2a77
SlickJohnson <liwa.johnson@gmail.com>
2018-03-02 13:01:34 -0800

    Layout nodes for game scene

---
+++ Dodga/GameNodes/Board.swift
@@ -0,0 +1,60 @@
+//
+//  Board.swift
+//  Dodga
+//
+//  Created by Willie Johnson on 3/2/18.
+//  Copyright © 2018 Willie Johnson. All rights reserved.
+//
+
+import SpriteKit
+
+/// The stripes of a board held in arrays that describe the stripes orientation.
+struct Stripes {
+  /// Stripes that are oriented horizontally.
+  var horizontal = [Stripe]()
+  /// Stripes that are oriented vertically.
+  var vertical = [Stripe]()
+}
+
+class Board: SKNode {
+  /// The horizontal and vertical stripes that makeup the board.
+  var stripes: Stripes!
+  /// The size of the board which will determine how long the stripes are.
+  var size: CGSize!
+
+  /// Creates a SKNode with Stripe nodes as its children.
+  init(size: CGSize) {
+    super.init()
+    self.size = size
+    make()
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: Useful public methods
+extension Board {
+  func make() {
+    stripes = Stripes()
+    // Make rows
+    for row in -1 ... 1 {
+      let stripe = Stripe(size: CGSize(width: size.width, height: 60), orientation: .horizontal)
+      addChild(stripe)
+      stripes.horizontal.append(stripe)
+      stripe.position.x = size.width / 2
+      stripe.position.y = size.height / 2 - CGFloat(65 * row)
+    }
+
+    // Make columns
+    for column in -1 ... 1 {
+      let stripe = Stripe(size: CGSize(width: 60, height: size.height), orientation: .vertical)
+      addChild(stripe)
+      stripes.vertical.append(stripe)
+      stripe.position.x = size.width / 2  - CGFloat(65 * column)
+      stripe.position.y = size.height / 2
+    }
+  }
+}
+
---
+++ Dodga/GameNodes/Player.swift
@@ -0,0 +1,19 @@
+//
+//  Player.swift
+//  Dodga
+//
+//  Created by Willie Johnson on 3/2/18.
+//  Copyright © 2018 Willie Johnson. All rights reserved.
+//
+
+import SpriteKit
+
+class Player: SKSpriteNode {
+  init() {
+    super.init(texture: nil, color: .red, size: CGSize(width: 40, height: 40))
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
